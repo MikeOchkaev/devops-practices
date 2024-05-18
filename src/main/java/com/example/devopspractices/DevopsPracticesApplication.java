45f96cf76eadcf33da211f913808286ee0f13201
MikeOhkaev <o4ckaev@mail.ru>
2024-05-18 13:56:47 +0300

    Initial commit

---
+++ src/main/java/com/example/devopspractices/DevopsPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopspractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsPracticesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsPracticesApplication.class, args);
+	}
+
+}
