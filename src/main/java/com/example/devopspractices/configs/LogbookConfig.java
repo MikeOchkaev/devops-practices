d2fff96b0898b7852810a4e351e3384922422438
MikeOhkaev <o4ckaev@mail.ru>
2024-06-22 13:40:25 +0300

    fixed logging

--- src/main/java/com/example/devopspractices/configs/LogbookConfig.java
+++ src/main/java/com/example/devopspractices/configs/LogbookConfig.java
@@ -2,10 +2,7 @@ package com.example.devopspractices.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.zalando.logbook.DefaultHttpLogWriter;
-import org.zalando.logbook.DefaultSink;
 import org.zalando.logbook.Logbook;
-import org.zalando.logbook.json.JsonHttpLogFormatter;
 
 
 @Configuration
@@ -13,9 +10,7 @@ public class LogbookConfig {
 
     @Bean
     public Logbook logbook() {
-        return Logbook.builder()
-                .sink(new DefaultSink(new JsonHttpLogFormatter(), new DefaultHttpLogWriter()))
-                .build();
+        return Logbook.create();
     }
 
 }
